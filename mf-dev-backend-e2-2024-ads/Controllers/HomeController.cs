b7bcb647a92e1a802fe49ace6a1549b139a463f0
sara-gracielle <sara.gracielle@sga.pucminas.br>
2024-10-13 17:51:31 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-backend-e2-2024-ads/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using mf_dev_backend_e2_2024_ads.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace mf_dev_backend_e2_2024_ads.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mf-dev-backend-e2-2024-ads/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mf_dev_backend_e2_2024_ads.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
