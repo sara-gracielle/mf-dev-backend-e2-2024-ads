d61b018fbd90045d4c0d6fe4404ec101531d98d4
sara-gracielle <sara.gracielle@sga.pucminas.br>
2024-10-13 18:25:24 -0300

    Configuração do Banco de Dados e da Tabela Veiculo

---
+++ mf-dev-backend-e2-2024-ads/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_e2_2024_ads.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
