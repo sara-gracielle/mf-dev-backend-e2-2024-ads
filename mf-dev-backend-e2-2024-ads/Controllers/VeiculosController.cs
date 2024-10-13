4f73bad23fe040a9d9a46436947de5c1e50e2c68
sara-gracielle <sara.gracielle@sga.pucminas.br>
2024-10-13 19:53:23 -0300

    Botão 'Delele'

--- mf-dev-backend-e2-2024-ads/Controllers/VeiculosController.cs
+++ mf-dev-backend-e2-2024-ads/Controllers/VeiculosController.cs
@@ -78,5 +78,36 @@ namespace mf_dev_backend_e2_2024_ads.Controllers
 
             return View(dados);
         }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            _context.Veiculos.Remove(dados);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
