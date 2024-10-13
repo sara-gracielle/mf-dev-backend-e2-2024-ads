ad8cfdef13c69b1a4c7ff202eda33ec04b25492e
sara-gracielle <sara.gracielle@sga.pucminas.br>
2024-10-13 18:45:58 -0300

    Criação do Controller 'Veiculo' e View

---
+++ mf-dev-backend-e2-2024-ads/Controllers/VeiculosController.cs
@@ -0,0 +1,23 @@
+﻿using mf_dev_backend_e2_2024_ads.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_e2_2024_ads.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+       public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+
+    }
+}
--- mf-dev-backend-e2-2024-ads/Models/Veiculo.cs
+++ mf-dev-backend-e2-2024-ads/Models/Veiculo.cs
@@ -17,10 +17,12 @@ namespace mf_dev_backend_e2_2024_ads.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        [Display(Name ="Ano de Fabricação")]
 
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        [Display(Name ="Ano do Modelo")]
 
         public int AnoModelo { get; set; }
     }
